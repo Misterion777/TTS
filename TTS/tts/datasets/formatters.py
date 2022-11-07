f3996e92c95f2c616280403fe6ce4ad4e879c23d
Ilya Lasy <ilyalas@gmail.com>
2022-11-07 13:09:18 +0200

    fixed few bugs

--- TTS/tts/datasets/formatters.py
+++ TTS/tts/datasets/formatters.py
@@ -225,7 +225,7 @@ def ruslan(root_path, meta_file, **kwargs):  # pylint: disable=unused-argument
     with open(txt_file, "r", encoding="utf-8") as ttf:
         for line in ttf:
             cols = line.split("|")
-            wav_file = os.path.join(root_path, "RUSLAN", cols[0] + ".wav")
+            wav_file = os.path.join(root_path, cols[0] + ".wav")
             text = cols[1]
             items.append({"text": text, "audio_file": wav_file, "speaker_name": speaker_name, "root_path": root_path})
     return items
